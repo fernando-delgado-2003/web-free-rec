92540e858cd96f3b4d8fd643c482d1f2fc41bff4
fernando-delgado-2003 <fernanbravo124@gmail.com>
2021-12-16 21:35:07 -0500

    b

---
+++ js/index.js
@@ -0,0 +1,42 @@
+function generateRandomInt(max) {
+	return Math.floor(Math.random() * max);
+}
+
+let contador = 25,
+	$counter = document.querySelector(".counter"),
+	intCursos = 31,
+	idRamdon = generateRandomInt(intCursos),
+	linkHref,
+	templateLinks;
+
+if (document.getElementById("index")) {
+	console.log("index")
+	if (idRamdon <= 0) {
+		idRamdon = 01;
+	} else if (idRamdon <= 9) {
+		idRamdon = `0${idRamdon}`;
+	}
+	linkHref = `https://web-free.vercel.app/course/?id=c_${idRamdon}`;
+	templateLinks = `
+			<a class="link" href="${linkHref}">Curso ramdon</a>
+			<a class="linkHome" href="https://web-free.vercel.app/">Ir ahora al inicio</a>
+	`;
+} else {
+	let title = document.querySelector("h1");
+	linkHref = `https://web-free.vercel.app/course/?id=c_${title.dataset.id}`;
+
+	templateLinks = `
+				<a class="link" href="${linkHref}">Ir al curso </a>
+	`;
+
+}
+document.querySelector(".links").innerHTML= templateLinks;
+setInterval(() => {
+	$counter.innerHTML = contador;
+	contador--
+	if (contador <= 0) {
+	let $a = document.createElement("a");
+	$a.href= linkHref;
+	$a.click()
+	}
+}, 1000)
